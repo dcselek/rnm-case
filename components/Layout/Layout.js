de48585b511000801f984eef8bf287fb6bd3fb0c
Doğan Can Selek <canselek2434@gmail.com>
2022-07-02 20:59:52 +0300

    update layout and index to change main page

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Head from './Head'
 
-export default function Layout({children, className}) {
+export default function Layout({ children, className }) {
   return (
     <div className={`w-full text-black ${className}`}>
-        <Head title='Rick & Morty' description='Rick & Morty' />
-        <main className='w-full'>
-            {children}
-        </main>
+      <div className='flex justify-center pt-6'>
+        <h1 className="text-5xl underline font-bold">Rick & Morty Case of For Software</h1>
+      </div>
+      <main className='w-full'>
+        {children}
+      </main>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,33 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Card from '../components/Card/Card'
-import Layout from '../components/Layout/Layout'
 import Pagination from '../components/Pagination/Pagination'
+import Head from '../components/Layout/Head'
 
 export default function Home() {
   const [characters, setCharacters] = useState(null)
   const [pageNum, setPageNum] = useState(1)
   useEffect(() => {
     axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNum}`).then(response => setCharacters(response.data.results))
-  }, [pageNum])  
+  }, [pageNum])
 
-  function callback (num) {
+  function callback(num) {
     setPageNum(num)
   }
 
   if (!characters) return "No post!"
 
   return (
-    <Layout>
-      <h1 className="text-3xl font-bold underline">Rick & Morty Case</h1>
+    <>
+      <Head title="Rick&Morty Case" description="Rick&Morty Case" />
       <ul className=" gap-10 flex flex-wrap pt-6 items-center justify-center">
-      {characters?.map(character => {
-        return <li key={character.id}>
-          <Card data={character} />
-        </li>
-      })}
+        {characters?.map(character => {
+          return <li key={character.id}>
+            <Card data={character} />
+          </li>
+        })}
       </ul>
       <Pagination onClick={callback} />
-    </Layout>
+    </>
   )
 }
