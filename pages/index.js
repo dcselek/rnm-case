39c944f4e784ced43eb7280c1dd65eb6a67e660d
Doğan Can Selek <canselek2434@gmail.com>
2022-07-02 15:45:28 +0300

    add axios example

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character').then(response => console.log(response))
+  })
+
   return (
     <div className={styles.container}>
       <Head>
