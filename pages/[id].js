9424f5fe2166f4aab4405d64c83969e6cd8b85a3
Doğan Can Selek <canselek2434@gmail.com>
2022-07-02 23:26:53 +0300

    add Head in id page

--- pages/[id].js
+++ pages/[id].js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Head from '../components/Layout/Head';
 
 export const getStaticPaths = async () => {
     const res = await axios.get('https://rickandmortyapi.com/api/character/');
@@ -32,28 +33,31 @@ export const getStaticProps = async (context) => {
 
 const Details = ({ character }) => {
     return (
-        <div className="flex flex-col justify-center items-center mx-auto py-5 w-1/2 border-2 mt-10 border-emerald-500 bg-emerald-50">
-            <img className="mx-auto" loading="lazy" src={`${character.image}`} alt={`${character.name}`} />
-            <span className="text-3xl font-bold text-red-400">{`${character.name}'s Details `}</span>
-            <p>
-                <b>Name: </b>{character.name}
-            </p>
-            <p>
-                <b>Status: </b>{character.status}
-            </p>
-            <p>
-                <b>Species: </b>{character.species}
-            </p>
-            <p>
-                <b>Gender: </b>{character.gender}
-            </p>
-            <p>
-                <b>Origin: </b>{character.origin.name}
-            </p>
-            <p>
-                <b>Location: </b>{character.location.name}
-            </p>
-        </div>
+        <>
+            <Head title={character.name} description={character.name} />
+            <div className="flex flex-col justify-center items-center mx-auto py-5 w-1/2 border-2 mt-10 border-emerald-500 bg-emerald-50">
+                <img className="mx-auto" loading="lazy" src={`${character.image}`} alt={`${character.name}`} />
+                <span className="text-3xl font-bold text-red-400">{`${character.name}'s Details `}</span>
+                <p>
+                    <b>Name: </b>{character.name}
+                </p>
+                <p>
+                    <b>Status: </b>{character.status}
+                </p>
+                <p>
+                    <b>Species: </b>{character.species}
+                </p>
+                <p>
+                    <b>Gender: </b>{character.gender}
+                </p>
+                <p>
+                    <b>Origin: </b>{character.origin.name}
+                </p>
+                <p>
+                    <b>Location: </b>{character.location.name}
+                </p>
+            </div>
+        </>
     );
 }
 
